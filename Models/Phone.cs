dd92090c1f8f632a5ad073c438b35f14364b5048
Anastasiia <nastyalukianiets09@gmail.com>
2024-06-25 11:07:05 +0300

    Init project
    Empty project ASP.NET Core MVC;
    Added 2 pages to web-site;
    Created Phone model;
    Realized transfer Model from Controller to View.

---
+++ Models/Phone.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication7.Models
+{
+    public class Phone
+    {
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string Price { get; set; }
+    }
+}
